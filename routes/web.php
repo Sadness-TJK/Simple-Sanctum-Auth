6340aef16770c469c3465b3d757c7774dbc90dfb
TJK <tomolol111@gmail.com>
2022-05-19 02:08:31 +0200

    something

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+/* GLOBAL */
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function (){
+    Route::post('/home/', function () {
+        return 'cool';
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
